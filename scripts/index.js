437b8b60e6f77f5e931a9232087bc7e403fb5075
Dmitriy Vasnev <trance0id@yandex.ru>
2022-12-26 13:40:58 +0300

    feature/addcard: added add card handler

--- scripts/index.js
+++ scripts/index.js
@@ -1,18 +1,27 @@
 const popupProfile = document.querySelector('.popup-edit-profile');
 const popupCard = document.querySelector('.popup-add-card');
+
 const buttonOpenEditProfile = document.querySelector('.profile__edit-button');
 const buttonCloseEditProfile = popupProfile.querySelector('.popup__close');
+
 const buttonOpenAddCard = document.querySelector('.profile__add-button');
 const buttonCloseAddCard = popupCard.querySelector('.popup__close');
+
 const nameField = document.querySelector('.profile__name');
-const aboutField = document.querySelector('.profile__description')
-const formEditProfile = popupProfile.querySelector('form[name="editprofile"]');
+const aboutField = document.querySelector('.profile__description');
+
+const formEditProfile = popupProfile.querySelector('form[name="edit-profile"]');
+const formAddCard = popupCard.querySelector('form[name="add-card"]');
+
 const nameInput = formEditProfile.querySelector('input[name="name"]');
 const aboutInput = formEditProfile.querySelector('input[name="about"]');
 
+const titleInput = formAddCard.querySelector('input[name="title"]');
+const urlInput = formAddCard.querySelector('input[name="url"]');
+
+
 const cardTemplate = document.querySelector('.card-template').content.querySelector('.card');
 const cardsContainer = document.querySelector('.cards__list');
-
 
 const initialCards = [
   {
@@ -43,43 +52,43 @@ const initialCards = [
 
 function createCard(card) {
   const newCard = cardTemplate.cloneNode(true);
-  newCard.querySelector('.card__caption-title').textContent = card['name'];
-  newCard.querySelector('.card__image').setAttribute('src', card['link']);
-  newCard.querySelector('.card__image').setAttribute('alt', 'Ещё одно интересное место');
+  newCard.querySelector('.card__caption-title').textContent = card.name;
+  newCard.querySelector('.card__image').setAttribute('src', card.link);
+  newCard.querySelector('.card__image').setAttribute('alt', `Фото места под названием ${card.name}`);
   return newCard;
 }
 
-function renderCards(cards) {
-  cards.forEach(card => {
-    cardsContainer.prepend(createCard(card));
-  });
+function renderCard(card) {
+  cardsContainer.prepend(createCard(card));
 }
 
 function openEditProfile() {
   nameInput.value = nameField.textContent;
   aboutInput.value = aboutField.textContent;
+  buttonCloseEditProfile.addEventListener('click', closeEditProfile);
+  formEditProfile.addEventListener('submit', handleEditProfileSubmit);
   popupProfile.classList.add('popup_opened');
 }
 
 function closeEditProfile() {
   popupProfile.classList.remove('popup_opened');
+  formEditProfile.removeEventListener('submit', handleEditProfileSubmit);
+  buttonCloseEditProfile.removeEventListener('click', closeEditProfile);
 }
 
 function openAddCard() {
+  titleInput.value = '';
+  urlInput.value = '';
+  formAddCard.addEventListener('submit', handleAddCardSubmit);
+  buttonCloseAddCard.addEventListener('click', closeAddCard);
   popupCard.classList.add('popup_opened');
 }
 
 function closeAddCard() {
   popupCard.classList.remove('popup_opened');
+  formAddCard.removeEventListener('submit', handleAddCardSubmit);
+  buttonCloseAddCard.removeEventListener('click', closeEditProfile);
 }
-
-buttonOpenEditProfile.addEventListener('click', openEditProfile);
-
-buttonCloseEditProfile.addEventListener('click', closeEditProfile);
-
-buttonOpenAddCard.addEventListener('click', openAddCard);
-
-buttonCloseAddCard.addEventListener('click', closeAddCard);
 
 function handleEditProfileSubmit(evt) {
   evt.preventDefault();
@@ -88,5 +97,14 @@ function handleEditProfileSubmit(evt) {
   closeEditProfile();
 }
 
-formEditProfile.addEventListener('submit', handleEditProfileSubmit);
-renderCards(initialCards);
+function handleAddCardSubmit(evt) {
+  evt.preventDefault();
+  const newCard = { name: titleInput.value || "Без названия", link: urlInput.value || "https://imageup.ru/img30/4138533/mepracticumjust-face.jpeg" };
+  renderCard(newCard);
+  closeAddCard();
+}
+
+buttonOpenEditProfile.addEventListener('click', openEditProfile);
+buttonOpenAddCard.addEventListener('click', openAddCard);
+
+initialCards.forEach(renderCard);
