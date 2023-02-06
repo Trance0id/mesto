35b804185817bae29bcfc58592f88fdf779a0f4c
Dmitriy Vasnev <trance0id@yandex.ru>
2023-02-06 12:55:54 +0300

    hotfix: added extensions in imports

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,9 +3,9 @@ import '../pages/index.css';
 import Card from "../components/Card.js";
 import FormValidator from "../components/FormValidator.js";
 import Section from "../components/Section.js";
-import PopupWithImage from '../components/PopupWithImage';
-import PopupWithForm from '../components/PopupWithForm';
-import UserInfo from '../components/UserInfo';
+import PopupWithImage from '../components/PopupWithImage.js';
+import PopupWithForm from '../components/PopupWithForm.js';
+import UserInfo from '../components/UserInfo.js';
 
 import { initialCards, validationConfig, buttonOpenAddCard, buttonOpenEditProfile } from '../utils/constants';
 
